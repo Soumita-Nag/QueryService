bb5cf8edac347cb2062d6335069b2c1b7bebed79
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-29 20:04:16 +0530

    css modified

--- src/components/HomePage/RecentQueriesHP.vue
+++ src/components/HomePage/RecentQueriesHP.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col items-center mt-10 sm:mx-5 mx-2 xl:mx-15 lg:mx-10">
+  <div class="flex flex-col items-center mt-10 sm:mx-5 mx-2 xl:mx-15 lg:mx-10 mb-20">
     <div><h1 class="text-3xl font-bold mb-6">Recent Queries</h1></div>
 
     <div v-if="!isloading" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
       <div v-for="query in allQueries" :key="query.queryId" class="bg-white shadow-md rounded-xl p-4 border border-gray-200 transition-all hover:scale-105 hover:shadow-xl cursor-pointer">
         <div class="text-sm text-gray-500 mb-2 flex justify-between ">
-          <span class="text-blue-700 font-semibold">{{ query.date }}, <span class="text-[0.7rem]">{{ query.time }}</span></span>
+          <span class="text-blue-700">{{ query.date }}, <span class="text-[0.7rem]">{{ query.time }}</span></span>
           <span><button class=" px-3 py-1 bg-blue-100 text-blue-700 text-[0.65rem] font-medium rounded-full">{{ query.category }}</button></span>
         </div>
         <div class="font-semibold mb-1">{{ query.queryTitle }}</div>
