29941cfa045fe8285948c761bbf3872f1d6ca2d3
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-29 20:00:31 +0530

    query design modified

--- src/components/HomePage/RecentQueriesHP.vue
+++ src/components/HomePage/RecentQueriesHP.vue
@@ -4,12 +4,34 @@
 
     <div v-if="!isloading" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
       <div v-for="query in allQueries" :key="query.queryId" class="bg-white shadow-md rounded-xl p-4 border border-gray-200 transition-all hover:scale-105 hover:shadow-xl cursor-pointer">
-        <div class="text-sm text-gray-500 mb-2">{{ query.date }}</div>
+        <div class="text-sm text-gray-500 mb-2 flex justify-between ">
+          <span class="text-blue-700 font-semibold">{{ query.date }}, <span class="text-[0.7rem]">{{ query.time }}</span></span>
+          <span><button class=" px-3 py-1 bg-blue-100 text-blue-700 text-[0.65rem] font-medium rounded-full">{{ query.category }}</button></span>
+        </div>
         <div class="font-semibold mb-1">{{ query.queryTitle }}</div>
         <div class="text-gray-700 text-sm truncate mb-2">
           Q. {{ query.query }}
         </div>
-        <div class="text-xs text-gray-500">Answers: {{ query.ansCount }}</div>
+        <div class="text-xs text-gray-500 flex gap-3">
+          <span class="flex gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-pink-500" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+            </svg>
+            {{ query.like }}
+          </span>
+          <span class="flex gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-green-600" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v10a2 2 0 002 2h10l4 4v-6a2 2 0 002-2z" />
+            </svg>
+            {{ query.ansCount }}
+          </span>
+          <span class="flex gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-blue-500" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M12 5c-7 0-11 7-11 7s4 7 11 7 11-7 11-7-4-7-11-7zm0 12a5 5 0 110-10 5 5 0 010 10zm0-8a3 3 0 100 6 3 3 0 000-6z"/>
+            </svg>
+            {{ query.views }}
+          </span>
+        </div>
       </div>
     </div>
     <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
